dd80ba54df22f7bf792d2e50c97456537e32f7a8
mitch344 <35354833+mitch344@users.noreply.github.com>
2024-06-05 10:46:26 -0700

    Adjust Partitions

--- lineage_os.py
+++ lineage_os.py
@@ -3,45 +3,46 @@ class LineageOS:
     fastboot_commands = [
         ("boot.img", "fastboot flash boot"),
         ("dtbo.img", "fastboot flash dtbo"),
+        ("vendor_boot.img", "fastboot flash vendor_boot"),
         ("vendor_kernel_boot.img", "fastboot flash vendor_kernel_boot")
     ]
 
     additional_partitions = {
         "flame": {
             "device_name": "Pixel 4",
-            "files": ["dtbo.img"]
+            "files": ["dtbo.img", "boot.img"]
         },
         "coral": {
             "device_name": "Pixel 4 XL",
-            "files": ["dtbo.img"]
+            "files": ["dtbo.img", "boot.img"]
         },
         "sunfish": {
             "device_name": "Pixel 4a",
-            "files": ["dtbo.img"]
+            "files": ["dtbo.img", "boot.img"]
         },
         "bramble": {
             "device_name": "Pixel 4a (5G)",
-            "files": ["boot.img", "dtbo.img"]
+            "files": ["boot.img", "dtbo.img", "vendor_boot.img"]
         },
         "redfin": {
             "device_name": "Pixel 5",
-            "files": ["boot.img", "dtbo.img"]
+            "files": ["boot.img", "dtbo.img", "vendor_boot.img"]
         },
         "barbet": {
             "device_name": "Pixel 5a (5G)",
-            "files": ["boot.img", "dtbo.img"]
+            "files": ["boot.img", "dtbo.img", "vendor_boot.img"]
         },
         "oriole": {
             "device_name": "Pixel 6",
-            "files": ["boot.img", "dtbo.img"]
+            "files": ["boot.img", "dtbo.img", "vendor_boot.img"]
         },
         "raven": {
             "device_name": "Pixel 6 Pro",
-            "files": ["boot.img", "dtbo.img"]
+            "files": ["boot.img", "dtbo.img", "vendor_boot.img"]
         },
         "bluejay": {
             "device_name": "Pixel 6a",
-            "files": ["boot.img", "dtbo.img"]
+            "files": ["boot.img", "dtbo.img", "vendor_boot.img"]
         },
         "panther": {
             "device_name": "Pixel 7",
