40f58f123f2fd086f5f670de025f06b20fadcc10
mitch344 <35354833+mitch344@users.noreply.github.com>
2024-06-04 20:33:45 -0700

    Fixed Script Refactor Bug

--- install_pipeline.py
+++ install_pipeline.py
@@ -10,21 +10,20 @@ class InstallPipeline:
         folder_path = os.path.join(current_directory, folder_name)
 
         if(self.channel == "LineageOS"):
-
-            return False #Place Holder
+            return False
 
         if platform.system() == "Windows":
             script_file = "flash-all.bat"
         else:
             script_file = "flash-all.sh"
 
-            script_path = self.find_script(folder_path, script_file)
+        script_path = self.find_script(folder_path, script_file)
 
-            if script_path:
-                os.system(f"cd {os.path.dirname(script_path)} && {script_file}")
-                return True
-            else:
-                return False
+        if script_path:
+            os.system(f"cd {os.path.dirname(script_path)} && {script_file}")
+            return True
+        else:
+            return False 
 
     def find_script(self, folder_path, script_name):
         for root, dirs, files in os.walk(folder_path):
